3fa3f6b591c174c7f9b9e8a83fe51156ac8055ae
John Smith <example@stanford.edu>
2013-07-12 06:36:27 +0000

    updated for HW3

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var urltest = require('url')
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -34,6 +36,12 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertValidURLPath = function(url) {
+    urltest.parse(url, false);
+    return url;
+
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,20 +63,69 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
+var validateUrl = function(checksfile){
+    var urlhandler = function(result, response){
+	if(result instanceof Error){
+	        //console.log(3);
+	        console.error('Error: ' + util.format(result));
+	        //console.log(4);
+	    } else {
+		    //console.log(5)
+		    //console.log(result);
+
+		    var checks = loadChecks(checksfile).sort();
+
+		    $ = cheerio.load(result);
+		    var out = {};
+		    for(var ii in checks) {
+			var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+			    }
+
+		    //var checks = loadChecks(checksfile).sort();
+		    //console.log("success..");
+
+		    var outJson = JSON.stringify(out, null, 4);
+		    console.log(outJson);
+		}
+    };
+    return urlhandler;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_link>', 'Url path', clone(assertValidURLPath))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    console.log(program.url);
+    console.log(program.checks);
+    console.log(program.file);
+
+    if(program.checks !== undefined && program.url !== undefined){
+	//      url check
+	var urlhandler = validateUrl(program.checks);
+
+	rest.get(program.url).on('complete', urlhandler);
+
+    }
+    else if(program.checks !== undefined && program.file !== undefined) {
+	//      file check
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    } else {
+	console.log("Invalid command usage.");
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
